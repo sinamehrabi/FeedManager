db4655d2b0134e4c897698a45db8b00c985fde5d
Sina Mehrabi <sina.mehrabi1990@gmail.com>
2021-08-09 12:16:18 +0430

    Add feed items DTOs to schema files in gateway and feed service

--- services/feeds/schema.py
+++ services/feeds/schema.py
@@ -1,23 +1,6 @@
-from typing import Optional, List, Union
+from typing import List, Optional, Union
 from datetime import datetime
-
 from pydantic import BaseModel
-
-
-class UserDTO(BaseModel):
-    email: str = None
-    password: Optional[str]
-    username: str = None
-    is_active: bool = None
-
-
-class Settings(BaseModel):
-    authjwt_secret_key: str = "secret"
-
-
-class AuthDTO(BaseModel):
-    access_token: str
-    refresh_token: str
 
 
 class FeedDTO(BaseModel):
@@ -26,10 +9,6 @@ class FeedDTO(BaseModel):
     link: str = None
     rss_link: str = None
     last_updated: Optional[Union[str, datetime]] = None
-
-
-class ListFeedDTO(BaseModel):
-    data: List[FeedDTO]
 
 
 class FeedItemDTO(BaseModel):
@@ -41,7 +20,3 @@ class FeedItemDTO(BaseModel):
     is_read: bool = False
     is_favorite: bool = False
     read_later: bool = False
-
-
-class ListFeedItemDTO(BaseModel):
-    data: List[FeedItemDTO]
